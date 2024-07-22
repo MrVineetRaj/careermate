88a7482b9a49b185a971fede6a28bde1190dc9b4
MrVineetRaj <vineetrajrj26@gmail.com>
2024-07-22 16:16:14 +0530

    back to puppeteer

--- next.config.mjs
+++ next.config.mjs
@@ -9,27 +9,6 @@ const nextConfig = {
       },
     ],
   },
-  webpack(config, options) {
-    // Exclude `chrome-aws-lambda` from being processed by Webpack
-    config.externals = config.externals || [];
-    config.externals.push('chrome-aws-lambda');
-
-    // Handle `puppeteer-core` module
-    config.module.rules.push({
-      test: /\.js$/,
-      exclude: /node_modules\/(?!puppeteer-core)/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env', '@babel/preset-react'],
-        },
-      },
-    });
-
-    // Add any other custom Webpack configuration here if needed
-
-    return config;
-  },
 };
 
 export default nextConfig;
--- src/app/api/generate-pdf/route.ts
+++ src/app/api/generate-pdf/route.ts
@@ -1,36 +1,57 @@
-import { NextResponse } from 'next/server';
-import { chromium } from 'playwright';
+import { NextResponse } from "next/server";
+import puppeteer from "puppeteer";
+// import chromium from "chrome-aws-lambda";
+// import puppeteer from "puppeteer-core";
+
 
 export async function POST(request: Request) {
   const reqObj = await request.json();
 
   try {
-    const browser = await chromium.launch({
+    
+    const browser = await puppeteer.launch({
       headless: true,
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      executablePath: puppeteer.executablePath(), // Ensure Puppeteer uses the bundled Chromium
     });
+
+  
+    // console.log("Chromium executable path:",await chromium.executablePath());
+    // const browser = await puppeteer.launch({
+    //   args: chromium.args,
+    //   defaultViewport: chromium.defaultViewport,
+    //   executablePath: await chromium.executablePath(),
+    //   headless: chromium.headless,
+    //   ignoreHTTPSErrors: true,
+    // });
 
     const page = await browser.newPage();
 
     await page.goto(
       `${process.env.NEXT_PUBLIC_BASE_URL}/share/resume?r=${reqObj.r}&u=${reqObj.u}`,
       {
-        waitUntil: 'networkidle',
+        waitUntil: "networkidle0",
       }
     );
 
-    await page.setViewportSize({ width: 595, height: 842 });
-    const pdfBuffer = await page.pdf({ format: 'A4', printBackground: true });
+    // Capture the PDF in memory
+    await page.setViewport({ width: 595, height: 842 });
+    const pdfBuffer = await page.pdf({ format: "a4", printBackground: true });
 
     await browser.close();
 
+    // Set response headers to indicate a file attachment
     const responseHeaders = new Headers();
-    responseHeaders.set('Content-Type', 'application/pdf');
-    responseHeaders.set('Content-Disposition', `attachment; filename="${reqObj.f}.pdf"`);
+    responseHeaders.set("Content-Type", "application/pdf");
+    responseHeaders.set(
+      "Content-Disposition",
+      `attachment; filename="${reqObj.f} "`
+    );
 
+    // Return the PDF buffer as a response
     return new NextResponse(pdfBuffer, { headers: responseHeaders });
-
   } catch (error: any) {
-    console.error('Error generating PDF:', error.message);
+    console.error("Error generating PDF:", error.message);
     return NextResponse.json({ success: false, error: error.message });
   }
 }
